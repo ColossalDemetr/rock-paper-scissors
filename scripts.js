f33434a31d1c5d48e8c048aa6d933da6d85ed964
Demetrius <241526608+ColossalDemetr@users.noreply.github.com>
2026-05-27 08:03:10 +0300

    Add Emmet into HTML and Pseudocode in .js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+/* 
+STEP ONE:    
+    1. Create a function getComputerChoice WITHOUT PARAMETERS
+    2. Gen a random number from 0 to 2(0,1,2)
+    3. If 0 - return rock
+    4. If 1 - return paper
+    5. ELSE - return scissors
+*/
+
